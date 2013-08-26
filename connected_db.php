40779c95dc1e3532cfb0222a838e11e25db48969
disraelligua <disraelligua@gmail.com>
2013-08-26 18:08:58 +0200

    Algunos cambios y mejoras

--- connected_db.php
+++ connected_db.php
@@ -1,6 +1,11 @@
 <?php
-	$cnx = mysql_connect('localhost',root,'');
+	$host = "localhost";
+	$name_db = "db_form_users";
+	$user_db = "root";
+	$psw_db = "";
+
+	$cnx = mysql_connect($host,$user_db,$psw_db) or die("Problemas al conectarse al host !!!");
 	if ($cnx) {
-		mysql_select_db('db_form_users',$cnx);
+		mysql_select_db($name_db,$cnx) or die("Problemas al conectare a la Base de Datos !!!");
 	}
 ?>
